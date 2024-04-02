69a7b52c382372f3506650ff84cd98d27ea0e5c3
Allan Philip Barku <allanbarku@gmail.com>
2024-04-02 08:27:04 +0000

    base setup for react with vite and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1
+        className="text-3xl text-center text-blue-500"
+      >Welcome to Vite with TailwindCSS and React</h1>
+    </>
+  )
+}
+
+export default App
